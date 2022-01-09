93cae3232daebdb2f73597e1fcc1d3ef2354e31c
sarojinisamyrajulu <sarogreens@gmail.com>
2022-01-09 19:01:44 +0530

    process done

---
+++ src/org/tcs/GitFirstSession.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class GitFirstSession {
+
+	public void add() {
+		System.out.println("sum");
+
+	}
+	public static void main(String[] args) {
+		GitFirstSession gf = new GitFirstSession();
+		gf.add();
+
+	}
+
+}
